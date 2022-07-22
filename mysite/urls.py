b260cacab9982c63d5f1c16747fa2a2e79bc5a70
Ramesh Pradhan <ramesh.pradhan@chuchuro.com>
2022-07-22 16:58:24 +0545

    Code refactor: Apply black formatting + Sort imports

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,13 +13,29 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path, include
 from django.contrib.auth import views
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
-    path('accounts/login', views.LoginView.as_view(), name='login'),
-    path('accounts/logout', views.LogoutView.as_view(), name='logout', kwargs={'next_page':'/'}),
+    path(
+        "admin/",
+        admin.site.urls,
+    ),
+    path(
+        "",
+        include("blog.urls"),
+    ),
+    path(
+        "accounts/login",
+        views.LoginView.as_view(),
+        name="login",
+    ),
+    path(
+        "accounts/logout",
+        views.LogoutView.as_view(),
+        name="logout",
+        kwargs={"next_page": "/"},
+    ),
 ]
